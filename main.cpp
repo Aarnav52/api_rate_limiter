f334bb3d7a7b63359275870cd595b7125ed5f864
Aarnav <aarnavpatel52@gmail.com>
2026-06-30 18:38:08 +0530

    Added chrono and mutex

--- main.cpp
+++ main.cpp
@@ -2,97 +2,99 @@
 #include<queue>
 #include<unordered_map>
 #include<string>
+#include<mutex>
+#include<chrono>
+
+using TimePoint = std::chrono::steady_clock::time_point;
 
 // config of a particular client with their limit and window 
 struct ClientConfig{
     long long limit;
-    long long window;
+    long long window; // interpreted as seconds
 };
-//queue keeping the tract of the requests made by the client 
+
+// queue keeping track of requests
 struct ClientState{
-    std::queue<long long> requests;
+    std::queue<TimePoint> requests;
 };
-// client structure keeping data of both clientconfig and clientstate
+
+// client structure
 struct Client{
     ClientConfig config;
     ClientState state;
 };
-//Api response 
+
+// Api response 
 struct ApiResponse{
     bool allowed;
     int statuscode;
     std::string message;
 };
+
 class SlidingWindowRateLimiter{
-    // a universal hash map where key is clientid or name and value is both structure clientconfig and clientstate
-    private:
+
+private:
     std::unordered_map<std::string,Client> clients;
+    std::mutex mtx;
 
-    public:
-    
+public:
+
     ApiResponse registerClient(const std::string &clientid,long long limit,long long window){
+        std::lock_guard<std::mutex> lock(mtx);
+
         if(!clients.count(clientid)){
-            clients[clientid].config.limit=limit;
-            clients[clientid].config.window=window;
-            return{
-                true,
-                200,
-                "Client Registered Successfully"
-            };
+            clients[clientid].config.limit = limit;
+            clients[clientid].config.window = window;
+            return {true, 200, "Client Registered Successfully"};
         }
-        else{
-            return{
-                false,
-                409,
-                "Client already exist"
-            };
+
+        return {false, 409, "Client already exists"};
+    }
+
+    bool validateClient(const std::string &clientid){
+        return clients.count(clientid);
+    }
+
+    void cleanupExpiredRequests(Client &client, TimePoint now){
+        //window is in integer format to clean up the queue we need to convert window into duration 
+        auto windowDuration = std::chrono::seconds(client.config.window);
+
+        while(!client.state.requests.empty() &&
+              client.state.requests.front() <= now - windowDuration){
+            client.state.requests.pop();
         }
     }
-    ////////////////////////////////////
-   
-   bool validateClient(const std::string &clientid) {
-    return clients.count(clientid);
-}
-   void cleanupExpiredRequests(Client &client, long long currenttime) {
-    while (!client.state.requests.empty() &&
-           client.state.requests.front() <= currenttime - client.config.window) {
-        client.state.requests.pop();
-    }
-}
-     bool isallowed(Client &client){
-        return client.state.requests.size()<client.config.limit;
-    }
-    void recordRequest(Client &client,long long currenttime){
-        client.state.requests.push(currenttime);
-    }
-    ////////////////////////////////////
-   ApiResponse allowRequest(const std::string &clientid, long long currenttime) {
 
-    if (!validateClient(clientid)) {
-        return {false, 404, "Client not Registered"};
+    bool isAllowed(Client &client){
+        return client.state.requests.size() < client.config.limit;
     }
 
-    Client &client = clients[clientid];
-
-    cleanupExpiredRequests(client, currenttime);
-
-    if (isallowed(client)) {
-        recordRequest(client, currenttime);
-        return {true, 200, "Request Accepted"};
+    void recordRequest(Client &client, TimePoint now){
+        client.state.requests.push(now);
     }
 
-    return {false, 429, "Too many requests"};
-}
+    ApiResponse allowRequest(const std::string &clientid){
+
+        std::lock_guard<std::mutex> lock(mtx);
+
+        if(!validateClient(clientid)){
+            return {false, 404, "Client not Registered"};
+        }
+
+        TimePoint now = std::chrono::steady_clock::now();
+
+        Client &client = clients[clientid];
+
+        cleanupExpiredRequests(client, now);
+
+        if(isAllowed(client)){
+            recordRequest(client, now);
+            return {true, 200, "Request Accepted"};
+        }
+
+        return {false, 429, "Too many requests"};
+    }
 };
 int main(){
-    SlidingWindowRateLimiter rl;
-    rl.registerClient("Aarnav",3,10);
-    for(int i=1;i<15;i++){
-        ApiResponse res=rl.allowRequest("Aarnav",i);
-        ApiResponse res1=rl.allowRequest("Aarnav",i);
 
-        std::cout<<"Time " << i<<res.message;
-        std::cout<<"Time " << i<<res1.message;
-        std::cout<<"\n";
-    }
 }
